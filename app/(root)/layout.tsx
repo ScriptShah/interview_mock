f3e71713b35fc8be1ed27370c752ffd56911ac40
ahmadshahdonishyar <ahmadshahdonishyar@gmail.com>
2025-04-14 10:25:33 +0430

    firebase Auth Function added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import React, { ReactNode } from "react";
 import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
-  const isUserAuthenticated = isAuthenticated;
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if(!isUserAuthenticated) redirect('/sign-in');
   return (
     <div className="root-layout">
       <nav>
